85af440a75bf90d6e0e7663abf457566fe6fec91
Gennaro De Luca <gdeluca1@asu.edu>
2015-01-08 11:57:06 -0700

    Ported over my DriveTrainTester class as well as the RobotMap, TPAJoystick, and TPARobotDrive classes. Updated Robot.java to call these classes.

--- So-crates1/src/org/usfirst/frc/team3944/robot/Robot.java
+++ So-crates1/src/org/usfirst/frc/team3944/robot/Robot.java
@@ -11,12 +11,21 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class Robot extends IterativeRobot {
+	private TPAJoystick joystick;
+    private TPARobotDrive robotDrive;
+    private DriveTrainTester driveTrainTester;
+    
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-
+    	joystick = new TPAJoystick(RobotMap.joystickOnePort);
+        robotDrive = new TPARobotDrive(RobotMap.frontLeftPort, RobotMap.rearLeftPort, RobotMap.frontRightPort, RobotMap.rearRightPort, joystick);
+        robotDrive.setInvertedMotor(TPARobotDrive.MotorType.kFrontRight, true);
+        robotDrive.setInvertedMotor(TPARobotDrive.MotorType.kRearRight, true);
+        
+        driveTrainTester = new DriveTrainTester(joystick, robotDrive);
     }
 
     /**
@@ -30,14 +39,19 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        
+    	robotDrive.mecanumDrive_Polar();
+    }
+    
+    @Override
+    public void testInit() {
+        driveTrainTester.reset();
     }
     
     /**
      * This function is called periodically during test mode
      */
     public void testPeriodic() {
-    
+    	driveTrainTester.run();
     }
     
 }
---
+++ So-crates1/src/org/usfirst/frc/team3944/robot/TPAJoystick.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team3944.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class TPAJoystick extends Joystick{
+	
+	private boolean[] buttonState = new boolean[11];
+    
+    public TPAJoystick(final int aPort) {
+        super(aPort);
+        for(int i=0; i<buttonState.length; i++) {
+            buttonState[i] = true;
+        }
+    }
+    
+    public boolean getButtonPush(int button) {
+        boolean returnValue;
+        // Call getRawButton from the Joystick class.
+        boolean newState = super.getRawButton(button);
+        if(newState == true && newState != buttonState[button-1]) {
+                returnValue = true;
+        }
+        else {returnValue = false;}
+        buttonState[button-1] = newState;
+        return(returnValue);
+    }
+}
